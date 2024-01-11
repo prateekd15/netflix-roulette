1607ce3938b3129efbc41fece578bc92b00d3fe5
Prateek Deshpande <prateekdeshpande11@outlook.com>
2024-01-11 17:00:04 +0530

    Added bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
-const path = require('path');
+const path = require("path");
+const DotenvWebpackPlugin = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
         {
-          test: /\.js$/, // Handle JavaScript/JSX files
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
           },
         },
         {
-          test: /\.css$/, // Handle CSS files
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.(png|jpe?g)$/i, // Handle .png and .jpg files
-          type: 'asset/resource',
+          test: /\.(png|jpe?g)$/i,
+          type: "asset/resource",
         },
       ],
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'), // specify the content base directory
     },
-    hot: true,
-    port: 3000,
-    open: true,
-  },
+    plugins: [
+        new DotenvWebpackPlugin(),
+        new BundleAnalyzerPlugin(),
+      ],
+    optimization: {
+    minimize: isProduction,
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+      },
+      hot: true,
+      port: 3000,
+      open: true,
+    },
+  };
 };
